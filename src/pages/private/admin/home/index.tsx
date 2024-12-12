53146a6858cae79e313cab08736ec76a44c57066
MTrazona <trazonamichael.pdm@gmail.com>
2024-12-12 17:31:42 +0800

    sda

--- src/pages/private/admin/home/index.tsx
+++ src/pages/private/admin/home/index.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from 'react';
-import { getFirestore, collection, getDocs, query, orderBy } from 'firebase/firestore';
-import { Table, Card, Statistic, Row, Col } from 'antd';
+import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore';
+import { Table, Card, Statistic, Row, Col, Select, DatePicker } from 'antd';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar, BarChart } from 'recharts';
 import { format } from 'date-fns';
 import { Category, Order } from '../../../../types';
+const { RangePicker } = DatePicker;
+const { Option } = Select;
 
 export const AdminHomepage = () => {
   const [totalIncome, setTotalIncome] = useState<number>(0);
@@ -12,89 +15,107 @@ export const AdminHomepage = () => {
   const [recentOrders, setRecentOrders] = useState<any[]>([]);
   const [productData, setProductData] = useState<any[]>([]);
   const [categoryData, setCategoryData] = useState<any[]>([]);
+  const [filterType, setFilterType] = useState<string>('daily');
+  const [selectedRange, setSelectedRange] = useState<any>(null);
 
   const db = getFirestore();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const ordersCollectionRef = collection(db, "orders");
-        const ordersSnapshot = await getDocs(ordersCollectionRef);
-        const ordersList = ordersSnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        })) as Order[];
-        ordersList.shift()
-        setOrderCount(ordersList.slice(1).length);
+  const fetchFilteredData = async () => {
+    try {
+      const ordersCollectionRef = collection(db, "orders");
+      let ordersQuery = query(ordersCollectionRef);
 
-        const totalIncomeValue = ordersList.reduce(
-          (sum, order: any) => Number(sum) + (Number(order.grandTotal) || 0),
-          0
-        );
-        setTotalIncome(totalIncomeValue);
-
-        const recentOrdersQuery = query(
+      if (selectedRange && selectedRange.length === 2) {
+        const [start, end] = selectedRange;
+        ordersQuery = query(
           ordersCollectionRef,
-          orderBy("timestamp", "desc")
+          where("timestamp", ">=", start.toDate()),
+          where("timestamp", "<=", end.toDate())
         );
-        const recentOrdersSnapshot = await getDocs(recentOrdersQuery);
-        const recentOrdersList = recentOrdersSnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        })) as Order[];
-        setRecentOrders(recentOrdersList.slice(0, 5));
-
-        const productSales: { [key: string]: number } = {};
-        ordersList.slice(1).forEach((order) => {
-          order.cartItems.forEach((item: any) => {
-            if (item.productName) { // Check if productName is defined
-              productSales[item.productName] =
-                (productSales[item.productName] || 0) +
-                item.quantity * item.price;
-            }
-          });
-        });
-        
-        const productData = Object.keys(productSales).map((productName) => ({
-          name: productName,
-          sales: productSales[productName],
-        }));
-        setProductData(productData);
-
-        const categoriesRef = collection(db, "categories");
-        const categoriesSnapshot = await getDocs(categoriesRef);
-        const categoriesList: Category[] = categoriesSnapshot.docs.map(
-          (doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          })
-        ) as Category[];
-        categoriesList.shift()
-        const categoryCounts: { [key: string]: number } = {};
-        ordersList.slice(1).forEach((order) => {
-          order.cartItems.forEach((item: any) => {
-            const category = categoriesList.find(
-              (v: Category) => v.name === item.category
-            )?.name;
-            if (category) {
-              categoryCounts[category] =
-                (categoryCounts[category] || 0) + item.quantity * item.price;
-            }
-          });
-        });
-
-        const categoryData = Object.keys(categoryCounts).map((category) => ({
-          name: category,
-          sales: categoryCounts[category],
-        }));
-        setCategoryData(categoryData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
       }
-    };
 
-    fetchData();
-  }, [db]);
+      const ordersSnapshot = await getDocs(ordersQuery);
+      const ordersList = ordersSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as Order[];
+
+      const totalIncomeValue = ordersList.reduce(
+        (sum, order: any) => Number(sum) + (Number(order.grandTotal) || 0),
+        0
+      );
+      setTotalIncome(totalIncomeValue);
+      setOrderCount(ordersList.length);
+
+      const recentOrdersList = ordersList
+      .sort((a:any, b:any) => {
+        const timeA = a.timestamp?.toDate ? a.timestamp.toDate().getTime() : 0; // Check if `toDate` exists
+        const timeB = b.timestamp?.toDate ? b.timestamp.toDate().getTime() : 0;
+        return timeB - timeA; // Sort by descending timestamp
+      })
+      .slice(0, 5);
+    setRecentOrders(recentOrdersList);
+    
+
+      const productSales: { [key: string]: number } = {};
+      ordersList.forEach((order) => {
+        order.cartItems.forEach((item: any) => {
+          if (item.productName) {
+            productSales[item.productName] =
+              (productSales[item.productName] || 0) +
+              item.quantity * item.price;
+          }
+        });
+      });
+
+      const productData = Object.keys(productSales).map((productName) => ({
+        name: productName,
+        sales: productSales[productName],
+      }));
+      setProductData(productData);
+
+      const categoriesRef = collection(db, "categories");
+      const categoriesSnapshot = await getDocs(categoriesRef);
+      const categoriesList: Category[] = categoriesSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as Category[];
+
+      const categoryCounts: { [key: string]: number } = {};
+      ordersList.forEach((order) => {
+        order.cartItems.forEach((item: any) => {
+          const category = categoriesList.find(
+            (v: Category) => v.name === item.category
+          )?.name;
+          if (category) {
+            categoryCounts[category] =
+              (categoryCounts[category] || 0) + item.quantity * item.price;
+          }
+        });
+      });
+
+      const categoryData = Object.keys(categoryCounts).map((category) => ({
+        name: category,
+        sales: categoryCounts[category],
+      }));
+      setCategoryData(categoryData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFilteredData();
+  }, [selectedRange]);
+
+  const handleFilterChange = (value: string) => {
+    setFilterType(value);
+    setSelectedRange(null);
+  };
+
+  const handleDateChange = (dates: any) => {
+    setSelectedRange(dates);
+  };
 
   const columns = [
     {
@@ -129,6 +150,30 @@ export const AdminHomepage = () => {
   return (
     <div className="min-h-screen p-4">
       <h2 className="text-3xl mb-4">Dashboard</h2>
+      <Row gutter={16} className="mb-4">
+        <Col span={8}>
+          <Select value={filterType} onChange={handleFilterChange} style={{ width: '100%' }}>
+            <Option value="daily">Daily</Option>
+            <Option value="weekly">Weekly</Option>
+            <Option value="monthly">Monthly</Option>
+            <Option value="yearly">Yearly</Option>
+          </Select>
+        </Col>
+        <Col span={16}>
+          {(filterType === 'weekly' || filterType === 'daily' || filterType === 'monthly' || filterType === 'yearly') && (
+            <RangePicker
+              picker={
+                filterType === 'monthly'
+                  ? 'month'
+                  : filterType === 'yearly'
+                  ? 'year'
+                  : undefined
+              }
+              onChange={handleDateChange}
+            />
+          )}
+        </Col>
+      </Row>
       <Row gutter={16} className="mb-4">
         <Col span={8}>
           <Card>
@@ -188,4 +233,3 @@ export const AdminHomepage = () => {
     </div>
   );
 };
-
